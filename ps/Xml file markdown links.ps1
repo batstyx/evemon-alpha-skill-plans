63731dd1f11ee62d3f1af935997016e48559f6bf
Batian Styx <batstyx@gmail.com>
2017-12-05 10:49:09 +0000

    Powershell scripts

---
+++ ps/Xml file markdown links.ps1
@@ -0,0 +1,6 @@
+﻿$path = Convert-Path .
+
+$folder = (Split-Path -Path $path -Leaf).Replace(" ", "%20")
+
+Get-ChildItem -Path $path | foreach-Object { "`t - ["  + $_.Name.substring(($_.Name.indexof("-") + 2), ($_.Name.indexof(".") - $_.Name.indexof("-") -2 )) + "](" + $folder + "/" + $_.Name.Replace(" ", "%20") + ")" }
+
